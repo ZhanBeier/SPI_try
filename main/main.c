cd1f07bc2aa6cfd96b8c1c6df36c0d70e1e857f8
ZZZBear <2411582954@qq.com>
2026-06-27 17:23:54 +0800

    v0.3

--- main/main.c
+++ main/main.c
@@ -1,6 +1,6 @@
 /**
  * @file      main.c
- * @brief     ADS1115 四通道 ADC 采集程序
+ * @brief     基于ESP32S3的BMS主控测试，本版本基于第一次测试板
  * @author    Bear Zhan
  * @date      2026-06-27
  * @version   V0.3
@@ -11,16 +11,23 @@
  *   AIN1 - 5V 电源监测
  *   AIN2 - 3.3V 电源监测
  *   AIN3 - NTC 热敏电阻
+ *   通过 CAN(ESP叫TWAI) 通信向外发送各自的转换值
+ *   header:0x00001234
+ *   注意每组各自的增益不尽相同，具体参考组件代码
  *
  * @hardware
- *   MCU:   ESP32-S3
- *   ADC:   ADS1115 (I²C Address: 0x48)
- *   SDA:   GPIO42
- *   SCL:   GPIO41
+ *   MCU:       ESP32-S3
+ *   ADC:       ADS1115 (I²C Address: 0x48)
+ *   SDA:       GPIO42
+ *   SCL:       GPIO41
+ *   CAN XCVR:  TJA1051T/3
+ *   CAN TX:    GPIO1
+ *   CAN RX:    GPIO2
  *
  * @changelog
  *   V0.1  2026-06-26  初始版本，验证ADS1115功能正常，验证Toggle Pin配USB-OTG调试正常
  *   V0.2  2026-06-26  在原来基础上修改了各通道的PGA值以更精确地测量；增加温度换算公式
+ *   V0.3  2026-06-27  重构工程，打包成独立组件；TJA1051T/3测试通过
  */
 #include <stdio.h>
 #include <math.h>
